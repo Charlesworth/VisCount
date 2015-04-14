810558b6bfafe4e49b8aaf70894c939985655924
Charlie <charlie@charlie-ThinkPad-W530.(none)>
2015-04-14 13:54:26 +0100

    added a mutex struct for IP and count writes and reads

--- viewCounter.go
+++ viewCounter.go
@@ -1,16 +1,28 @@
 package main
 
 import (
-	"encoding/json"
+	//"encoding/json"
 	"fmt"
-	"github.com/boltdb/bolt"
+	//"github.com/boltdb/bolt"
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
+	"runtime"
+	"sync"
 )
 
 var BoltReadChannel = make(chan string)
 var BoltWriteChannel = make(chan DataPoint)
+
+var counter = struct {
+	sync.RWMutex
+	m map[string]int
+}{m: make(map[string]int)}
+
+var ips = struct {
+	sync.RWMutex
+	m map[string]bool
+}{m: make(map[string]bool)}
 
 type DataPoint struct {
 	PageName    string
@@ -35,6 +47,10 @@ type DataPoint struct {
 
 func main() {
 
+	procNo := runtime.NumCPU()
+	runtime.GOMAXPROCS(procNo)
+	fmt.Println("Using", procNo, "processors for maximum thread count")
+
 	router := httprouter.New()
 	router.GET("/count/:pageID", countHandler)
 	router.GET("/stats/:pswrd", statsHandler)
@@ -47,13 +63,19 @@ func main() {
 
 func countHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	log.Println(r.RemoteAddr + " requests " + params.ByName("pageID"))
-	//either
-	//update ip map with mutex
-	//add pageID atomic counter
 
-	//or
-	//ipLogger (bufferred channel) <- IP address
-	//viewLogger (buffered channel) <- page ID
+	counter.Lock()
+	counter.m[params.ByName("pageID")]++
+	counter.Unlock()
+
+	// counter.RLock()
+	// n := counter.m[params.ByName("pageID")]
+	// counter.RUnlock()
+	// log.Println(params.ByName("pageID"), n)
+
+	ips.Lock()
+	ips.m[r.RemoteAddr] = true
+	ips.Unlock()
 }
 
 func statsHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
@@ -79,60 +101,60 @@ func errLog(err error) {
 	}
 }
 
-func boltWriteClient() {
-	boltClient, err := bolt.Open("viewCounter.db", 0600, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer boltClient.Close()
+// func boltWriteClient() {
+// 	boltClient, err := bolt.Open("viewCounter.db", 0600, nil)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer boltClient.Close()
 
-	//do we need to check a bucket exists or make one
-	boltClient.Update(func(tx *bolt.Tx) error {
-		// Create a bucket.
-		tx.CreateBucketIfNotExists([]byte("m"))
-		return nil
-	})
+// 	//do we need to check a bucket exists or make one
+// 	boltClient.Update(func(tx *bolt.Tx) error {
+// 		// Create a bucket.
+// 		tx.CreateBucketIfNotExists([]byte("m"))
+// 		return nil
+// 	})
 
-	fmt.Println("bolt writer ready")
+// 	fmt.Println("bolt writer ready")
 
-	for {
+// 	for {
 
-		m := <-BoltWriteChannel
-		mjson, err := json.Marshal(m)
-		errLog(err)
-		boltClient.Update(func(tx *bolt.Tx) error {
-			// Set the value "bar" for the key "foo".
-			err = tx.Bucket([]byte("m")).Put([]byte("poo"), []byte(mjson)) //need the bucket id for this
-			errLog(err)
-			return nil
-		})
+// 		m := <-BoltWriteChannel
+// 		mjson, err := json.Marshal(m)
+// 		errLog(err)
+// 		boltClient.Update(func(tx *bolt.Tx) error {
+// 			// Set the value "bar" for the key "foo".
+// 			err = tx.Bucket([]byte("m")).Put([]byte("poo"), []byte(mjson)) //need the bucket id for this
+// 			errLog(err)
+// 			return nil
+// 		})
 
-	}
-}
+// 	}
+// }
 
-func boltReadClient() {
-	boltClient, err := bolt.Open("viewCounter.db", 0600, nil) //maybe change the 600 to a read only value
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer boltClient.Close()
+// func boltReadClient() {
+// 	boltClient, err := bolt.Open("viewCounter.db", 0600, nil) //maybe change the 600 to a read only value
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer boltClient.Close()
 
-	fmt.Println("bolt reader ready")
+// 	fmt.Println("bolt reader ready")
 
-	for {
-		id := <-BoltReadChannel
+// 	for {
+// 		id := <-BoltReadChannel
 
-		var b []byte
-		boltClient.View(func(tx *bolt.Tx) error {
-			// Set the value "bar" for the key "foo".
-			b = tx.Bucket([]byte("m")).Get([]byte(id))
-			errLog(err)
+// 		var b []byte
+// 		boltClient.View(func(tx *bolt.Tx) error {
+// 			// Set the value "bar" for the key "foo".
+// 			b = tx.Bucket([]byte("m")).Get([]byte(id))
+// 			errLog(err)
 
-			return nil
-		})
+// 			return nil
+// 		})
 
-		//var mjson Message
-		//err := json.Unmarshal(b, &mjson)
+// 		//var mjson Message
+// 		//err := json.Unmarshal(b, &mjson)
 
-	}
-}
+// 	}
+// }
